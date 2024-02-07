4e11ca57c644856dd455dbda70612f193ec44d25
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-02-07 21:53:36 +0300

    star rating

---
+++ src/StarRating.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "18px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "1",
+};
+const starStyle = {
+  width: "48px",
+  height: "48px",
+  display: "block",
+  cursor: "pointer",
+  color: "yellow",
+};
+
+export default function StarRating({ maxRating = 5 }) {
+  // setting default value if user does not select a rating
+  const [rating, setRating] = useState(1);
+
+  //handling rating nd setting the value to setRating
+  function handleRating(rating) {
+    setRating(rating);
+  }
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star key={i} onRate={() => handleRating(i + 1)} />
+        ))}
+        <p style={textStyle}>{rating || ""}</p>
+      </div>
+    </div>
+  );
+}
+
+function Star({ onRate }) {
+  return (
+    <span style={starStyle} role="button" onClick={onRate}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="#000"
+        stroke="#000"
+      >
+        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+      </svg>
+    </span>
+  );
+}
