2694a1dca70be88ca075826f2862a17eb9292fde
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-02-16 17:43:36 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
-// import "./index.css"
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
 
-    {/* <App /> */}
+    <Test /> */}
   </React.StrictMode>
 );
